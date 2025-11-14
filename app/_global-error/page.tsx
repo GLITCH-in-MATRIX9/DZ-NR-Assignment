a483db11b8b25184834fc02060e6720478745d1f
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2025-11-15 02:50:46 +0530

    Update page.tsx

--- app/_global-error/page.tsx
+++ app/_global-error/page.tsx
@@ -1,14 +1,7 @@
-"use client"; // at the very top
-
-import { useAuth } from "../../context/AuthContext";
-
 export default function GlobalErrorPage() {
-  const { user } = useAuth();
-
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
       <h1>Something went wrong!</h1>
-      {user && <p>Hello, {user.name}</p>}
     </div>
   );
 }
